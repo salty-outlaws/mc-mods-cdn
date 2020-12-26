272601e977e9e409c5773ce9dafc2be421622d2b
varunbheemaiah <32514558+varunbheemaiah@users.noreply.github.com>
2020-12-26 16:57:41 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,9 @@
 cd /data/mods/
 wget https://salty-outlaws.github.io/mc-mods-cdn/AutoRegLib-1.6-47.jar
 wget https://salty-outlaws.github.io/mc-mods-cdn/ImmersiveEngineering-1.16.4-4.1.2-129.jar
-wget https://salty-outlaws.github.io/mc-mods-cdn/OptiFine-OptiFine-1.16.4_HD_U_G5.jar
 wget https://salty-outlaws.github.io/mc-mods-cdn/Quark-r2.4-283.jar
 wget https://salty-outlaws.github.io/mc-mods-cdn/QuarkOddities-1.16.3.jar
 wget https://salty-outlaws.github.io/mc-mods-cdn/Xaeros_Minimap_20.29.1_Forge_1.16.4.jar
 wget https://salty-outlaws.github.io/mc-mods-cdn/buildinggadgets-1.16.4-3.7.3.jar
 wget https://salty-outlaws.github.io/mc-mods-cdn/chiselsandbits-0.2.8-RELEASE.jar
-wget https://salty-outlaws.github.io/mc-mods-cdn/tl_skin_cape_forge_1.16.4-1.16.jar
 wget https://salty-outlaws.github.io/mc-mods-cdn/worldedit-forge-mc1.16.3-7.2.1-dist.jar
