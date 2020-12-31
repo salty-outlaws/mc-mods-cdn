9c47c484ad7ed6ac219e321655595831e66a7d53
varunbheemaiah <32514558+varunbheemaiah@users.noreply.github.com>
2020-12-31 22:49:00 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ wget https://salty-outlaws.github.io/mc-mods-cdn/ImmersiveEngineering-1.16.4-4.1
 wget https://salty-outlaws.github.io/mc-mods-cdn/Quark-r2.4-283.jar
 wget https://salty-outlaws.github.io/mc-mods-cdn/QuarkOddities-1.16.3.jar
 wget https://salty-outlaws.github.io/mc-mods-cdn/Xaeros_Minimap_20.30.1_Forge_1.16.4.jar
-wget https://salty-outlaws.github.io/mc-mods-cdn/buildinggadgets-1.16.4-3.7.3.jar\
+wget https://salty-outlaws.github.io/mc-mods-cdn/buildinggadgets-1.16.4-3.7.3.jar
 wget https://salty-outlaws.github.io/mc-mods-cdn/worldedit-forge-mc1.16.3-7.2.1-dist.jar
